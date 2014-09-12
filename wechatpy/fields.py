1ad05335e259ed59194e24615b69eca7eb3c305a
messense <messense@icloud.com>
2014-09-12 23:30:07 +0800

    Add some more fields for replies

--- wechatpy/fields.py
+++ wechatpy/fields.py
@@ -38,3 +38,103 @@ class FloatField(BaseField):
         value = self.converter(value)
         tpl = '<{name}>{value}</{name}>'
         return tpl.format(name=self.name, value=value)
+
+
+class ImageField(StringField):
+
+    def to_xml(self, value):
+        value = self.converter(value)
+        tpl = '<Image>\n<MediaId>![CDATA[{value}]]</MediaId>\n</Image>'
+        return tpl.format(value=value)
+
+
+class VoiceField(StringField):
+
+    def to_xml(self, value):
+        value = self.converter(value)
+        tpl = '<Voice>\n<MediaId>![CDATA[{value}]]</MediaId>\n</Voice>'
+        return tpl.format(value=value)
+
+
+class VideoField(StringField):
+
+    def to_xml(self, media_id, title=None, description=None):
+        media_id = self.converter(media_id)
+        if title:
+            title = self.converter(title)
+        if description:
+            description = self.converter(description)
+        tpl = """<Video>
+        <MediaId>![CDATA[{media_id}]]</MediaId>
+        <Title>![CDATA[{title}]]</Title>
+        <Description>![CDATA[{description}]]</Description>
+        </Video>
+        """
+        return tpl.format(
+            media_id=media_id,
+            title=title,
+            description=description
+        )
+
+
+class MusicField(StringField):
+
+    def to_xml(self, thumb_media_id, title=None, description=None,
+               music_url=None, hq_music_url=None):
+        thumb_media_id = self.converter(thumb_media_id)
+        if title:
+            title = self.converter(title)
+        if description:
+            description = self.converter(description)
+        if music_url:
+            music_url = self.converter(music_url)
+        if hq_music_url:
+            hq_music_url = self.converter(hq_music_url)
+        tpl = """<Music>
+        <ThumbMediaId>![CDATA[{thumb_media_id}]]</ThumbMediaId>
+        <Title>![CDATA[{title}]]</Title>
+        <Description>![CDATA[{description}]]</Description>
+        <MusicUrl>![CDATA[{music_url}]]</MusicUrl>
+        <HQMusicUrl>![CDATA[{hq_music_url}]]</HQMusicUrl>
+        </Music>"""
+        return tpl.format(
+            thumb_media_id=thumb_media_id,
+            title=title,
+            description=description,
+            music_url=music_url,
+            hq_music_url=hq_music_url
+        )
+
+
+class ArticleField(StringField):
+
+    def to_xml(self, articles):
+        article_count = len(articles)
+        items = []
+        for article in articles:
+            title = self.converter(article.get('title', ''))
+            description = self.converter(article.get('description', ''))
+            image = self.converter(article.get('image', ''))
+            url = self.converter(article.get('url', ''))
+            item_tpl = """<item>
+            <Title>![CDATA[{title}]]</Title>
+            <Description>![CDATA[{description}]]</Description>
+            <PicUrl>![CDATA[{image}]]</PicUrl>
+            <Url>![CDATA[{url}]]</Url>
+            </item>
+            """
+            item = item_tpl.format(
+                title=title,
+                description=description,
+                image=image,
+                url=url
+            )
+            items.append(item)
+        items_str = '\n'.join(items)
+        tpl = """<ArticleCount>{article_count}</ArticleCount>
+        <Articles>{items}</Articles>
+        """
+        return tpl.format(
+            article_count=article_count,
+            items=items_str
+        )
