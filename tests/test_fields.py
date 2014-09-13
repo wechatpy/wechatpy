1463ad33c01ddb52c961c0b00be0fcae1aaf03f1
messense <messense@icloud.com>
2014-09-13 14:49:58 +0800

    Use unittest2 for Python 2.6 compatibility

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-import unittest
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
 
 class FieldsTestCase(unittest.TestCase):
