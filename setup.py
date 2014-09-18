f19acd8439b644c87fc56bdb6d7eddd559ffad9a
messense <messense@icloud.com>
2014-09-18 09:15:01 +0800

    Test Python 3.2 support

--- setup.py
+++ setup.py
@@ -36,6 +36,11 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ],
 )
