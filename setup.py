1cba665baaa72c3d029569ba4a3332112cd1edcd
messense <messense@icloud.com>
2014-09-23 17:29:34 +0800

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='wechatpy',
-    version='0.0.9',
+    version='0.1.0',
     author='messense',
     author_email='messense@icloud.com',
     url='https://github.com/messense/wechatpy',
--- wechatpy/__init__.py
+++ wechatpy/__init__.py
@@ -6,5 +6,5 @@ from .client import WeChatClient  # NOQA
 from .exceptions import WeChatException  # NOQA
 
 
-__version__ = '0.0.9'
+__version__ = '0.1.0'
 __author__ = 'messense'
