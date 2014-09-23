99558689d9ce399ded604ad6398173e297841fc3
messense <messense@icloud.com>
2014-09-23 14:28:49 +0800

    Update echo-enterprise example

--- examples/echo-enterprise/app.py
+++ examples/echo-enterprise/app.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 from flask import Flask, request, abort
 from wechatpy.enterprise.crypto import WeChatCrypto
 from wechatpy.exceptions import InvalidSignatureException
+from wechatpy.enterprise.exceptions import InvalidCorpIdException
 
 
 TOKEN = '123456'
@@ -16,23 +17,26 @@ def wechat():
     signature = request.args.get('msg_signature', '')
     timestamp = request.args.get('timestamp', '')
     nonce = request.args.get('nonce', '')
-    echo_str = request.args.get('echostr', '')
 
     crypto = WeChatCrypto(TOKEN, EncodingAESKey, CorpId)
-    try:
-        echo_str = crypto.check_signature(
-            signature,
-            timestamp,
-            nonce,
-            echo_str
-        )
-    except InvalidSignatureException:
-        abort(403)
     if request.method == 'GET':
+        echo_str = request.args.get('echostr', '')
+        try:
+            echo_str = crypto.check_signature(
+                signature,
+                timestamp,
+                nonce,
+                echo_str
+            )
+        except InvalidSignatureException:
+            abort(403)
         return echo_str
     else:
-        print('test')
-        return ''
+        try:
+            msg = crypto.decrypt_message(request.data)
+        except (InvalidSignatureException, InvalidCorpIdException):
+            abort(403)
+        print msg
 
 
 if __name__ == '__main__':
