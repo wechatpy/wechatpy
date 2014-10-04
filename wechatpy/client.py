da331b08777e9bdef6d3426fb09f6b0e7fb5ae9c
messense <messense@icloud.com>
2014-10-04 11:39:26 +0800

    Add WeChat ticket APIs

--- wechatpy/client.py
+++ wechatpy/client.py
@@ -546,7 +546,7 @@ class WeChatClient(BaseWeChatClient):
             params=params
         )
 
-    def create_qrcode(self, **data):
+    def create_qrcode(self, qrcode_data):
         """
         创建二维码
         详情请参考 http://mp.weixin.qq.com/wiki/index.php?title=生成带参数的二维码
@@ -556,7 +556,7 @@ class WeChatClient(BaseWeChatClient):
         """
         return self._post(
             'qrcode/create',
-            data=data
+            data=qrcode_data
         )
 
     def show_qrcode(self, ticket):
@@ -824,4 +824,267 @@ class WeChatClient(BaseWeChatClient):
         return self._post(
             'customservice/getrecord',
             data=record_data
+        )
+
+    def create_card(self, card_data):
+        """
+        创建卡券
+        """
+        return self._post(
+            'card/create',
+            data=card_data
+        )
+
+    def batch_add_locations(self, location_data):
+        """
+        批量导入门店信息
+        """
+        return self._post(
+            'card/location/batchadd',
+            data=location_data
+        )
+
+    def batch_get_locations(self, offset=0, count=0):
+        """
+        批量获取门店信息
+        """
+        return self._post(
+            'card/location/batchget',
+            data={
+                'offset': offset,
+                'count': count
+            }
+        )
+
+    def get_card_colors(self):
+        """
+        获得卡券的最新颜色列表，用于创建卡券
+        """
+        return self._get('card/getcolors')
+
+    def create_card_qrcode(self, qrcode_data):
+        """
+        创建卡券二维码
+        """
+        return self._post(
+            'card/qrcode/create',
+            data=qrcode_data
+        )
+
+    def consume_card_code(self, code, card_id=None):
+        """
+        消耗 code
+        """
+        card_data = {
+            'code': code
+        }
+        if card_id:
+            card_data['card_id'] = card_id
+        return self._post(
+            'card/code/consume',
+            data=card_data
+        )
+
+    def decrypt_card_code(self, encrypt_code):
+        """
+        解码加密的 code
+        """
+        return self._post(
+            'card/code/decrypt',
+            data={
+                'encrypt_code': encrypt_code
+            }
+        )
+
+    def delete_card(self, card_id):
+        """
+        删除卡券
+        """
+        return self._post(
+            'card/delete',
+            data={
+                'card_id': card_id
+            }
+        )
+
+    def get_card_code(self, code):
+        """
+        查询 code 信息
+        """
+        return self._post(
+            'card/code/get',
+            data={
+                'code': code
+            }
+        )
+
+    def batch_get_cards(self, offset=0, count=50):
+        """
+        批量查询卡券信息
+        """
+        return self._post(
+            'card/batchget',
+            data={
+                'offset': offset,
+                'count': count
+            }
+        )
+
+    def get_card(self, card_id):
+        """
+        查询卡券详情
+        """
+        return self._post(
+            'card/get',
+            data={
+                'card_id': card_id
+            }
+        )
+
+    def update_card_code(self, card_id, old_code, new_code):
+        """
+        更新卡券 code
+        """
+        return self._post(
+            'card/code/update',
+            data={
+                'card_id': card_id,
+                'code': old_code,
+                'new_code': new_code
+            }
+        )
+
+    def invalid_card_code(self, code, card_id=None):
+        """
+        设置卡券失效
+        """
+        card_data = {
+            'code': code
+        }
+        if card_id:
+            card_data['card_id'] = card_id
+        return self._post(
+            'card/code/unavailable',
+            data=card_data
+        )
+
+    def update_card(self, card_data):
+        """
+        更新卡券信息
+        """
+        return self._post(
+            'card/update',
+            data=card_data
+        )
+
+    def set_card_test_whitelist(self, openids=None, usernames=None):
+        """
+        设置卡券测试用户白名单
+        """
+        openids = openids or []
+        usernames = usernames or []
+        return self._post(
+            'card/testwhitelist/set',
+            data={
+                'openid': openids,
+                'username': usernames
+            }
+        )
+
+    def activate_membercard(self, membership_number, code, init_bonus=0,
+                            init_balance=0, card_id=None):
+        """
+        激活/绑定会员卡
+        """
+        card_data = {
+            'membership_number': membership_number,
+            'code': code,
+            'init_bonus': init_bonus,
+            'init_balance': init_balance
+        }
+        if card_id:
+            card_data['card_id'] = card_id
+        return self._post(
+            'card/membercard/activate',
+            data=card_data
+        )
+
+    def update_membercard(self, code, add_bonus=0, record_bonus='',
+                          add_balance=0, record_balance='', card_id=None):
+        """
+        会员卡交易更新信息
+        """
+        card_data = {
+            'code': code,
+            'add_bonus': add_bonus,
+            'add_balance': add_balance,
+            'record_bonus': record_bonus,
+            'record_balance': record_balance
+        }
+        if card_id:
+            card_data['card_id'] = card_id
+        return self._post(
+            'card/membercard/updateuser',
+            data=card_data
+        )
+
+    def update_movie_ticket(self, code, ticket_class, show_time, duration,
+                            screening_room, seat_number, card_id=None):
+        """
+        更新电影票
+        """
+        ticket = {
+            'code': code,
+            'ticket_class': ticket_class,
+            'show_time': show_time,
+            'duration': duration,
+            'screening_room': screening_room,
+            'seat_number': seat_number
+        }
+        if card_id:
+            ticket['card_id'] = card_id
+        return self._post(
+            'card/movieticket/updateuser',
+            data=ticket
+        )
+
+    def checkin_boardingpass(self, code, passenger_name, seat_class,
+                             etkt_bnr, seat='', gate='', boarding_time=None,
+                             is_cancel=False, qrcode_data=None, card_id=None):
+        """
+        飞机票接口
+        """
+        data = {
+            'code': code,
+            'passenger_name': passenger_name,
+            'class': seat_class,
+            'etkt_bnr': etkt_bnr,
+            'seat': seat,
+            'gate': gate,
+            'is_cancel': is_cancel
+        }
+        if boarding_time:
+            data['boarding_time'] = boarding_time
+        if qrcode_data:
+            data['qrcode_data'] = qrcode_data
+        if card_id:
+            data['card_id'] = card_id
+        return self._post(
+            'card/boardingpass/checkin',
+            data=data
+        )
+
+    def update_luckymoney_balance(self, code, balance, card_id=None):
+        """
+        更新红包余额
+        """
+        card_data = {
+            'code': code,
+            'balance': balance
+        }
+        if card_id:
+            card_data['card_id'] = card_id
+        return self._post(
+            'card/luckymoney/updateuserbalance',
+            data=card_data
         )
