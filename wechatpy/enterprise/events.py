e3ab0251f1b36a1b390239800e53b38c11fa3c01
messense <messense@icloud.com>
2014-10-16 10:59:17 +0800

    Add more events for enterprise, tests not pass now, will fix it latter. [ci skip]

--- wechatpy/enterprise/events.py
+++ wechatpy/enterprise/events.py
@@ -36,3 +36,33 @@ class ViewEvent(events.ViewEvent):
 @register_event('location')
 class LocationEvent(events.LocationEvent):
     agent = IntegerField('AgentID', 0)
+
+
+@register_event('scancode_push')
+class ScanCodePushEvent(events.ScanCodePushEvent):
+    agent = IntegerField('AgentID', 0)
+
+
+@register_event('scancode_waitmsg')
+class ScanCodeWaitMsgEvent(events.ScanCodeWaitMsgEvent):
+    agent = IntegerField('AgentID', 0)
+
+
+@register_event('pic_sysphoto')
+class PicSysPhotoEvent(events.PicSysPhotoEvent):
+    agent = IntegerField('AgentID', 0)
+
+
+@register_event('pic_photo_or_album')
+class PicPhotoOrAlbumEvent(events.PicPhotoOrAlbumEvent):
+    agent = IntegerField('AgentID', 0)
+
+
+@register_event('pic_weixin')
+class PicWeChatEvent(events.PicWeChatEvent):
+    agent = IntegerField('AgentID', 0)
+
+
+@register_event('location_select')
+class LocationSelectEvent(events.LocationSelectEvent):
+    agent = IntegerField('AgentID', 0)
