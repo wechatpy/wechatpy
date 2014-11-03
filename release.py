b0b5e2586d13f39a8f2982ca604528a73eef95f4
messense <messense@icloud.com>
2014-11-03 13:34:24 +0800

    Update release script: use pypandoc instead of pyandoc [ci skip]

--- release.py
+++ release.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, with_statement
-import os
 import sys
 
 try:
-    import pandoc
+    import pypandoc
 except ImportError:
-    print('Please install pyandoc first, run pip install pyandoc.')
+    print('Please install pypandoc first, run pip install pypandoc.')
     sys.exit()
 
-pandoc_path = os.popen('which pandoc').read().strip()
-if not pandoc_path:
-    print('Cannot find pandoc executable file, you should install pandoc.')
-    sys.exit()
-
-pandoc.core.PANDOC_PATH = pandoc_path
-
-doc = pandoc.Document()
-with open('README.md') as f:
-    doc.markdown = f.read()
+rst = pypandoc.convert('README.md', 'rst')
 with open('README.rst', 'w+') as f:
-    f.write(doc.rst)
-os.system("python setup.py release")
-os.remove('README.rst')
+    f.write(rst.encode('utf-8'))
+# os.system("python setup.py release")
+# os.remove('README.rst')
