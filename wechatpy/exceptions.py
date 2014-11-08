4d4e963193df0f8c0d69e0da08711c90a0b879ab
messense <messense@icloud.com>
2014-11-08 18:15:59 +0800

    Add docstrings for wechatpy.exceptions module [ci skip]

--- wechatpy/exceptions.py
+++ wechatpy/exceptions.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    wechatpy.exceptions
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Basic exceptions definition.
+
+    :copyright: (c) 2014 by messense.
+    :license: MIT, see LICENSE for more details.
+"""
 from __future__ import absolute_import, unicode_literals
 import six
 
@@ -5,8 +15,13 @@ from .utils import to_binary, to_text
 
 
 class WeChatException(Exception):
+    """Base exception for wechatpy"""
 
     def __init__(self, errcode, errmsg):
+        """
+        :param errcode: Error code
+        :param errmsg: Error message
+        """
         self.errcode = errcode
         self.errmsg = errmsg
 
@@ -24,22 +39,26 @@ class WeChatException(Exception):
 
 
 class WeChatClientException(WeChatException):
+    """WeChat API client exception class"""
     pass
 
 
 class InvalidSignatureException(WeChatException):
+    """Invalid signature exception class"""
 
     def __init__(self, errcode=-40001, errmsg='Invalid signature'):
         super(InvalidSignatureException, self).__init__(errcode, errmsg)
 
 
 class APILimitedException(WeChatException):
+    """WeChat API call limited exception class"""
 
     def __init__(self, errcode=45009, errmsg='api freq out of limit'):
         super(APILimitedException, self).__init__(errcode, errmsg)
 
 
 class InvalidAppIdException(WeChatException):
+    """Invalid app_id exception class"""
 
     def __init__(self, errcode=-40005, errmsg='Invalid AppId'):
         super(InvalidAppIdException, self).__init__(errcode, errmsg)
