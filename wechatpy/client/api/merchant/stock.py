2102d7f0ef40d2dbebb13a4a9823d4e6ab02f899
messense <messense@icloud.com>
2014-11-17 21:46:38 +0800

    Add merchant stock API

---
+++ wechatpy/client/api/merchant/stock.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from wechatpy.client.api.base import BaseWeChatAPI
+
+
+class MerchantStock(BaseWeChatAPI):
+
+    def add(self, product_id, quantity, sku_info=''):
+        return self._post(
+            'merchant/stock/add',
+            data={
+                'product_id': product_id,
+                'quantity': quantity,
+                'sku_info': sku_info
+            }
+        )
+
+    def reduce(self, product_id, quantity, sku_info=''):
+        return self._post(
+            'merchant/stock/reduce',
+            data={
+                'product_id': product_id,
+                'quantity': quantity,
+                'sku_info': sku_info
+            }
+        )
