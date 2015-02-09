8b2fb2ce1c86c2d6abefe67060576173b87efaaf
messense <messense@icloud.com>
2015-02-09 20:47:32 +0800

    Use dict.get instead of in

--- wechatpy/parser.py
+++ wechatpy/parser.py
@@ -27,8 +27,7 @@ def parse_message(xml):
     message_type = message['MsgType'].lower()
     if message_type == 'event':
         event_type = message['Event'].lower()
-        if event_type == 'subscribe' and 'EventKey' in message and \
-                message['EventKey']:
+        if event_type == 'subscribe' and message.get('EventKey'):
             # Scan to subscribe with scene id event
             event_type = 'subscribe_scan'
             message['Event'] = event_type
