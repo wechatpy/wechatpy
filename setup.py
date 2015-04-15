d14976e2ab8ef97db0f761571a2200191c3fdaf4
messense <messense@icloud.com>
2015-04-15 16:31:54 +0800

    Update README badges. [ci skip]

--- setup.py
+++ setup.py
@@ -38,6 +38,11 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
     ],
 )
