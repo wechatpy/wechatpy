a7fed4ffacda4dd0c1404d526d22414dc02587b8
messense <messense@icloud.com>
2015-05-03 22:34:13 +0800

    [New]Add new JS-SDK example `wxjs`. [ci skip]

---
+++ examples/wxjs/main/views.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import time
+
+from django.conf import settings
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+
+from wechatpy import WeChatClient
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+@csrf_exempt
+def jsapi_signature(request):
+    noncestr = '123456'
+    timestamp = int(time.time())
+    url = request.POST['url']
+
+    client = WeChatClient(settings.WECHAT_APPID, settings.WECHAT_SECRET)
+    ticket = client.jsapi.get_ticket()
+    signature = client.jsapi.get_jsapi_signature(
+        noncestr,
+        ticket['ticket'],
+        timestamp,
+        url
+    )
+    ret_dict = {
+        'noncestr': noncestr,
+        'timestamp': timestamp,
+        'url': url,
+        'signature': signature,
+    }
+    return JsonResponse(ret_dict)
+
+
+def log(request):
+    print('Hello World!')
+    return JsonResponse({
+        'status': 'ok',
+    })
