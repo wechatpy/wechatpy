cf07a468a4b3dd61d7de21296170f1f35e1538cb
steven zhao <wentao79@gmail.com>
2015-09-14 14:03:47 +0800

    WeChatComponentClient extends from WeChatClient

--- wechatpy/client/__init__.py
+++ wechatpy/client/__init__.py
@@ -5,72 +5,6 @@ from wechatpy.client.base import BaseWeChatClient
 from wechatpy.client import api
 
 
-class WeChatComponentClient(BaseWeChatClient):
-
-    """
-    开放平台代公众号调用客户端
-    """
-    API_BASE_URL = 'https://api.weixin.qq.com/cgi-bin/'
-
-    menu = api.WeChatMenu()
-    user = api.WeChatUser()
-    group = api.WeChatGroup()
-    media = api.WeChatMedia()
-    card = api.WeChatCard()
-    qrcode = api.WeChatQRCode()
-    message = api.WeChatMessage()
-    misc = api.WeChatMisc()
-    merchant = api.WeChatMerchant()
-    customservice = api.WeChatCustomService()
-    datacube = api.WeChatDataCube()
-    jsapi = api.WeChatJSAPI()
-    material = api.WeChatMaterial()
-    semantic = api.WeChatSemantic()
-    shakearound = api.WeChatShakeAround()
-    device = api.WeChatDevice()
-    template = api.WeChatTemplate()
-    poi = api.WeChatPoi()
-
-    def __init__(
-            self, appid, access_token, refresh_token, component, session=None):
-        super(WeChatComponentClient, self).__init__(access_token, session)
-        self.appid = appid
-        self.component = component
-        self.session.set('refresh_token', refresh_token)
-
-    @property
-    def access_token(self):
-        access_token = self.session.get('access_token')
-        if not access_token:
-            self.fetch_access_token()
-            access_token = self.session.get('access_token')
-        return access_token
-
-    @property
-    def refresh_token(self):
-        return self.session.get('refresh_token')
-
-    def fetch_access_token(self):
-        """
-        获取 access token
-        详情请参考 https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list\
-        &t=resource/res_list&verify=1&id=open1419318587&token=&lang=zh_CN
-
-        :return: 返回的 JSON 数据包
-        """
-        expires_in = 7200
-        result = self.component.refresh_authorizer_token(
-            self.appid, self.refresh_token)
-        if 'expires_in' in result:
-            expires_in = result['expires_in']
-        self.session.set('access_token', result[
-                         'authorizer_access_token'], expires_in)
-        self.session.set('refresh_token', result[
-                         'authorizer_refresh_token'], expires_in)
-        self.expires_at = int(time.time()) + expires_in
-        return result
-
-
 class WeChatClient(BaseWeChatClient):
 
     """
@@ -119,3 +53,51 @@ class WeChatClient(BaseWeChatClient):
                 'secret': self.secret
             }
         )
+
+
+class WeChatComponentClient(WeChatClient):
+
+    """
+    开放平台代公众号调用客户端
+    """
+
+    def __init__(
+            self, appid, access_token, refresh_token, component, session=None):
+        # 未用到secret，所以这里没有
+        super(WeChatComponentClient, self).__init__(
+            appid, '', access_token, session)
+        self.appid = appid
+        self.component = component
+        self.session.set('refresh_token', refresh_token)
+
+    @property
+    def access_token(self):
+        access_token = self.session.get('access_token')
+        if not access_token:
+            self.fetch_access_token()
+            access_token = self.session.get('access_token')
+        return access_token
+
+    @property
+    def refresh_token(self):
+        return self.session.get('refresh_token')
+
+    def fetch_access_token(self):
+        """
+        获取 access token
+        详情请参考 https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list\
+        &t=resource/res_list&verify=1&id=open1419318587&token=&lang=zh_CN
+
+        :return: 返回的 JSON 数据包
+        """
+        expires_in = 7200
+        result = self.component.refresh_authorizer_token(
+            self.appid, self.refresh_token)
+        if 'expires_in' in result:
+            expires_in = result['expires_in']
+        self.session.set(
+            'access_token', result['authorizer_access_token'], expires_in)
+        self.session.set(
+            'refresh_token', result['authorizer_refresh_token'], expires_in)
+        self.expires_at = int(time.time()) + expires_in
+        return result
