49ac29a94b0f564c2252e61c56f89bceffcd3651
messense <messense@icloud.com>
2015-09-17 17:20:37 +0800

    Update echo example to support both plaintext and encrypted mode

--- examples/echo/app.py
+++ examples/echo/app.py
@@ -1,12 +1,19 @@
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import os
+
 from flask import Flask, request, abort, render_template
 from wechatpy import parse_message, create_reply
 from wechatpy.utils import check_signature
-from wechatpy.exceptions import InvalidSignatureException
+from wechatpy.exceptions import (
+    InvalidSignatureException,
+    InvalidAppIdException,
+)
 
 # set token or get from environments
 TOKEN = os.getenv('WECHAT_TOKEN', '123456')
+AES_KEY = os.getenv('WECHAT_AES_KEY', '')
+APPID = os.getenv('WECHAT_APPID', '')
 
 app = Flask(__name__)
 
@@ -22,20 +29,46 @@ def wechat():
     signature = request.args.get('signature', '')
     timestamp = request.args.get('timestamp', '')
     nonce = request.args.get('nonce', '')
-    echo_str = request.args.get('echostr', '')
+    encrypt_type = request.args.get('encrypt_type', '')
+    msg_signature = request.args.get('msg_signature', '')
     try:
         check_signature(TOKEN, signature, timestamp, nonce)
     except InvalidSignatureException:
         abort(403)
     if request.method == 'GET':
+        echo_str = request.args.get('echostr', '')
         return echo_str
-    else:
+
+    # POST request
+    if encrypt_type == 'raw':
+        # plaintext mode
         msg = parse_message(request.data)
         if msg.type == 'text':
             reply = create_reply(msg.content, msg)
         else:
             reply = create_reply('Sorry, can not handle this for now', msg)
         return reply.render()
+    else:
+        # encryption mode
+        from wechatpy.crypto import WeChatCrypto
+
+        crypto = WeChatCrypto(TOKEN, AES_KEY, APPID)
+        try:
+            msg = crypto.decrypt_message(
+                request.data,
+                msg_signature,
+                timestamp,
+                nonce
+            )
+        except (InvalidSignatureException, InvalidAppIdException):
+            abort(403)
+        else:
+            msg = parse_message(msg)
+            if msg.type == 'text':
+                reply = create_reply(msg.content, msg)
+            else:
+                reply = create_reply('Sorry, can not handle this for now', msg)
+            return crypto.encrypt_message(reply.render(), nonce, timestamp)
 
 
 if __name__ == '__main__':
