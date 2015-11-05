d7605e65217eaee61ce3294bfaefa7677d764f65
林十四 <jeremylam14@gmail.com>
2015-11-05 10:01:05 +0800

    Update __init__.py
    新增微信刷卡接口

--- wechatpy/pay/api/__init__.py
+++ wechatpy/pay/api/__init__.py
@@ -7,3 +7,4 @@ from wechatpy.pay.api.order import WeChatOrder  # NOQA
 from wechatpy.pay.api.refund import WeChatRefund  # NOQA
 from wechatpy.pay.api.tools import WeChatTools  # NOQA
 from wechatpy.pay.api.jsapi import WeChatJSAPI  # NOQA
+from wechatpy.pay.api.micropay import WeChatMicroPay  # NOQA
