fb9a2f207be5a8d355431a0a01e0622e60c5d968
messense <messense@icloud.com>
2015-12-17 16:08:47 +0800

    Bump version to v1.2.2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='wechatpy',
-    version='1.2.1',
+    version='1.2.2',
     author='messense',
     author_email='messense@icloud.com',
     url='https://github.com/messense/wechatpy',
--- wechatpy/__init__.py
+++ wechatpy/__init__.py
@@ -12,5 +12,5 @@ from wechatpy.exceptions import WeChatPayException  # NOQA
 from wechatpy.component import WeChatComponent  # NOQA
 
 
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 __author__ = 'messense'
