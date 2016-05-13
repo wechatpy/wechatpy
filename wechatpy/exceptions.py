bf1258fdcabf66f30ece1bf104711d3368bb6197
messense <messense@icloud.com>
2016-05-13 23:31:03 +0800

    Better `__repr__` and `__str__` for WeChatPayException

--- wechatpy/exceptions.py
+++ wechatpy/exceptions.py
@@ -105,3 +105,26 @@ class WeChatPayException(WeChatClientException):
         self.return_code = return_code
         self.result_code = result_code
         self.return_msg = return_msg
+
+    def __str__(self):
+        if six.PY2:
+            return to_binary('Error code: {code}, message: {msg}'.format(
+                code=self.return_code,
+                msg=self.return_msg
+            ))
+        else:
+            return to_text('Error code: {code}, message: {msg}'.format(
+                code=self.return_code,
+                msg=self.return_msg
+            ))
+
+    def __repr__(self):
+        _repr = '{klass}({code}, {msg})'.format(
+            klass=self.__class__.__name__,
+            code=self.return_code,
+            msg=self.return_msg
+        )
+        if six.PY2:
+            return to_binary(_repr)
+        else:
+            return to_text(_repr)
