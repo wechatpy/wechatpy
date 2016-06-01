dc5d2d256bd196009749e1900a6b79a89094547c
messense <messense@icloud.com>
2016-06-01 11:24:58 +0800

    Bump version: 1.2.10 → 1.2.11

--- wechatpy/__init__.py
+++ wechatpy/__init__.py
@@ -18,5 +18,5 @@ from wechatpy.exceptions import WeChatPayException  # NOQA
 from wechatpy.component import WeChatComponent  # NOQA
 
 
-__version__ = '1.2.10'
+__version__ = '1.2.11'
 __author__ = 'messense'
