4b3330b47779dd17836bdfe8c1474bb8104d7892
messense <messense@icloud.com>
2017-01-02 11:22:03 +0800

    Happy New Year

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'wechatpy'
-copyright = u'2014 - 2016, Messense Lv'
+copyright = u'2014 - 2017, Messense Lv'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
