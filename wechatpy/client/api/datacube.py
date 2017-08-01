c31f99b6823f3f6bc9ed7b9e980c02668884bacb
quxiaolong <quxiaolong@yun-ji.cn>
2017-08-01 17:20:38 +0800

    修改 datacube 膨胀的问x

--- wechatpy/client/api/datacube.py
+++ wechatpy/client/api/datacube.py
@@ -6,6 +6,20 @@ import six
 
 from wechatpy.client.api.base import BaseWeChatAPI
 
+"""
+用户分析数据接口: https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141082
+图文分析数据接口: https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141084
+消息分析数据接口: https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141085
+接口分析数据接口: https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141086
+小程分析数据接口: https://mp.weixin.qq.com/debug/wxadoc/dev/api/analysis.html
+
+# How to usage:
+from wechatpy import WeChatClient
+client = WeChatClient(appid="your app id", secret="your secret")
+result = client.datacube.getweanalysisappiddailysummarytrend(begin_time="2017-07-31", end_time="2017-08-01")
+result = client.datacube.getusersummary(begin_time="2017-07-31", end_time="2017-08-01")
+"""
+
 
 class WeChatDataCube(BaseWeChatAPI):
 
@@ -20,341 +34,23 @@ class WeChatDataCube(BaseWeChatAPI):
         else:
             raise ValueError('Can not convert %s type to str', type(date))
 
-    def get_user_summary(self, begin_date, end_date):
-        """
-        获取用户增减数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/3/ecfed6e1a0a03b5f35e5efac98e864b7.html
+    def __getattr__(self, item):
+        return DataCubeObject(item, self)
 
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getusersummary',
+
+class DataCubeObject(object):
+    def __init__(self, name, proxy):
+        self.name = name
+        self.proxy = proxy
+
+    def __call__(self, *args, **kwargs):
+        begin_date = kwargs.get('begin_date', None)
+        end_date = kwargs.get('end_date', None)
+        res = self.proxy._post(
+            self.name,
             data={
                 'begin_date': self._to_date_str(begin_date),
                 'end_date': self._to_date_str(end_date)
             }
         )
-        return res['list']
-
-    def get_user_cumulate(self, begin_date, end_date):
-        """
-        获取累计用户数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/3/ecfed6e1a0a03b5f35e5efac98e864b7.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getusercumulate',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_interface_summary(self, begin_date, end_date):
-        """
-        获取接口分析数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/8/30ed81ae38cf4f977194bf1a5db73668.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getinterfacesummary',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_interface_summary_hour(self, begin_date, end_date):
-        """
-        获取接口分析分时数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/8/30ed81ae38cf4f977194bf1a5db73668.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getinterfacesummaryhour',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_article_summary(self, begin_date, end_date):
-        """
-        获取图文群发每日数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/8/c0453610fb5131d1fcb17b4e87c82050.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getarticlesummary',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_article_total(self, begin_date, end_date):
-        """
-        获取图文群发总数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/8/c0453610fb5131d1fcb17b4e87c82050.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getarticletotal',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_user_read(self, begin_date, end_date):
-        """
-        获取图文统计数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/8/c0453610fb5131d1fcb17b4e87c82050.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getuserread',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_user_read_hour(self, begin_date, end_date):
-        """
-        获取图文分时统计数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/8/c0453610fb5131d1fcb17b4e87c82050.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getuserreadhour',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_user_share(self, begin_date, end_date):
-        """
-        获取图文分享转发数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/8/c0453610fb5131d1fcb17b4e87c82050.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getusershare',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_user_share_hour(self, begin_date, end_date):
-        """
-        获取图文分享转发分时数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/8/c0453610fb5131d1fcb17b4e87c82050.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getusersharehour',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_upstream_msg(self, begin_date, end_date):
-        """
-        获取消息发送概况数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/12/32d42ad542f2e4fc8a8aa60e1bce9838.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getupstreammsg',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_upstream_msg_hour(self, begin_date, end_date):
-        """
-        获取消息发送分时数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/12/32d42ad542f2e4fc8a8aa60e1bce9838.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getupstreammsghour',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_upstream_msg_week(self, begin_date, end_date):
-        """
-        获取消息发送周数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/12/32d42ad542f2e4fc8a8aa60e1bce9838.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getupstreammsgweek',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_upstream_msg_month(self, begin_date, end_date):
-        """
-        获取消息发送月数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/12/32d42ad542f2e4fc8a8aa60e1bce9838.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getupstreammsgmonth',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_upstream_msg_dist(self, begin_date, end_date):
-        """
-        获取消息发送分布数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/12/32d42ad542f2e4fc8a8aa60e1bce9838.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getupstreammsgdist',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_upstream_msg_dist_week(self, begin_date, end_date):
-        """
-        获取消息发送分布数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/12/32d42ad542f2e4fc8a8aa60e1bce9838.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getupstreammsgdistweek',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
-
-    def get_upstream_msg_dist_month(self, begin_date, end_date):
-        """
-        获取消息发送分布数据
-        详情请参考
-        http://mp.weixin.qq.com/wiki/12/32d42ad542f2e4fc8a8aa60e1bce9838.html
-
-        :param begin_date: 起始日期
-        :param end_date: 结束日期
-        :return: 统计数据列表
-        """
-        res = self._post(
-            'getupstreammsgdistmonth',
-            data={
-                'begin_date': self._to_date_str(begin_date),
-                'end_date': self._to_date_str(end_date)
-            },
-            result_processor=lambda x: x['list']
-        )
-        return res
+        return res['list'] if 'list' in res else res
