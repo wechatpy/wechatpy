2816ce955fd648ca294358eb2dfe6ac1324df6ad
messense <messense@icloud.com>
2017-08-02 13:40:27 +0800

    Bump version: 1.5.0 → 1.5.1

--- wechatpy/__init__.py
+++ wechatpy/__init__.py
@@ -14,7 +14,7 @@ from wechatpy.exceptions import WeChatPayException  # NOQA
 from wechatpy.component import WeChatComponent  # NOQA
 
 
-__version__ = '1.5.0'
+__version__ = '1.5.1'
 __author__ = 'messense'
 
 # Set default logging handler to avoid "No handler found" warnings.
