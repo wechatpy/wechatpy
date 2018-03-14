f71fea710215d4b9483e904e26c64df9c6f8111f
filosfino <zheduanyin@163.com>
2018-03-14 14:39:30 +0800

    增加对沙盒支付的支持、针对不同前端的支持 (#345)
    * 为 pay/jsapi/get_jsapi_params 添加不同前端调用方式的支持
    
    * 为 pay/jsapi 添加沙箱模式支持
    
    * format code

--- wechatpy/pay/api/jsapi.py
+++ wechatpy/pay/api/jsapi.py
@@ -25,15 +25,21 @@ class WeChatJSAPI(BaseWeChatPayAPI):
             'signType': 'MD5',
             'package': 'prepay_id={0}'.format(prepay_id),
         }
-        return calculate_signature(data, self._client.api_key)
+        return calculate_signature(
+            data,
+            self._client.api_key if not self._client.sandbox else self._client.sandbox_api_key
+        )
 
-    def get_jsapi_params(self, prepay_id, timestamp=None, nonce_str=None):
+    def get_jsapi_params(self, prepay_id, timestamp=None, nonce_str=None, jssdk=False):
         """
         获取 JSAPI 参数
 
         :param prepay_id: 统一下单接口返回的 prepay_id 参数值
         :param timestamp: 可选，时间戳，默认为当前时间戳
         :param nonce_str: 可选，随机字符串，默认自动生成
+        :param jssdk: 前端调用方式，默认使用 WeixinJSBridge
+                      使用 jssdk 调起支付的话，timestamp 的 s 为小写
+                      使用 WeixinJSBridge 调起支付的话，timeStamp 的 S 为大写
         :return: 参数
         """
         data = {
@@ -43,6 +49,11 @@ class WeChatJSAPI(BaseWeChatPayAPI):
             'signType': 'MD5',
             'package': 'prepay_id={0}'.format(prepay_id),
         }
-        sign = calculate_signature(data, self._client.api_key)
+        sign = calculate_signature(
+            data,
+            self._client.api_key if not self._client.sandbox else self._client.sandbox_api_key
+        )
         data['paySign'] = sign
+        if jssdk:
+            data['timestamp'] = data.pop('timeStamp')
         return data
