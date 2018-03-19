83fa857099f5a73ac9ee629c16e391f7de6362dd
messense <messense@icloud.com>
2018-03-19 11:15:41 +0800

    Bump version: 1.6.0 → 1.6.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='wechatpy',
-    version='1.6.0',
+    version='1.6.1',
     author='messense',
     author_email='messense@icloud.com',
     url='https://github.com/messense/wechatpy',
