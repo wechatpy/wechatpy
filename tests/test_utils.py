037d1b579522d8a40af26463854fa190fb56ca1b
filosfino <filosfino@gmail.com>
2018-04-04 13:26:07 +0800

    fix: rsa encrypt/decrypt now outputs binary

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -67,4 +67,4 @@ class UtilityTestCase(unittest.TestCase):
         with open(os.path.join(_CERTS_PATH, 'rsa_public_key.pem'), 'rb') as public_fp, \
                 open(os.path.join(_CERTS_PATH, 'rsa_private_key.pem'), 'rb') as private_fp:
             encrypted_string = rsa_encrypt(target_string, public_fp.read(), b64_encode=False)
-            self.assertEqual(rsa_decrypt(encrypted_string, private_fp.read()), target_string)
+            self.assertEqual(rsa_decrypt(encrypted_string, private_fp.read()), target_string.encode('utf-8'))
