0d2b80604928e7ae68a218d0289f081a5b0999ee
messense <messense@icloud.com>
2019-01-07 14:46:40 +0800

    Drop Python 3.4 support

--- setup.py
+++ setup.py
@@ -70,7 +70,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
