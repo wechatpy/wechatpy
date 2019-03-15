d9c39c4cf5b6b12b32dad6c0a58140b4c60414e8
gitwell <thinkweiwei@msn.com>
2019-03-15 15:17:09 +0800

    feat (wxa) : decrypt WechatMiniprogram user data.

--- wechatpy/crypto/cryptography.py
+++ wechatpy/crypto/cryptography.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import, unicode_literals
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
+from Crypto.Cipher import AES
+import json
+
 
 class WeChatCipher(object):
 
@@ -21,3 +24,19 @@ class WeChatCipher(object):
     def decrypt(self, ciphertext):
         decryptor = self.cipher.decryptor()
         return decryptor.update(ciphertext) + decryptor.finalize()
+
+
+class WeChatWxaCipher(object):
+
+    def __init__(self, key, iv):
+        self.cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    def encrypt(self, plain_data):
+        pass
+
+    def decrypt(self, cipher_data):
+        decrypted = json.loads(self._unpad(self.cipher.decrypt(cipher_data)))
+        return decrypted
+
+    def _unpad(self, s):
+        return s[:-ord(s[len(s) - 1:])]
