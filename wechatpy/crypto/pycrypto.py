be2f9d438d696e0c8043b73c5778ddf6abc020a6
messense <messense@icloud.com>
2019-03-16 15:24:30 +0800

    微信小程序开放数据解密
    Thanks @SeverusWell for PR #428
    
    Closes #428

--- wechatpy/crypto/pycrypto.py
+++ wechatpy/crypto/pycrypto.py
@@ -5,8 +5,9 @@ from Crypto.Cipher import AES
 
 class WeChatCipher(object):
 
-    def __init__(self, key):
-        self.cipher = AES.new(key, AES.MODE_CBC, key[:16])
+    def __init__(self, key, iv=None):
+        iv = iv or key[:16]
+        self.cipher = AES.new(key, AES.MODE_CBC, iv)
 
     def encrypt(self, plaintext):
         return self.cipher.encrypt(plaintext)
