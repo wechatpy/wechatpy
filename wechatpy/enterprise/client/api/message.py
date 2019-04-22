f08671866a1d2ef630414beeade86f16b54b7c3e
Huiming <Kairu_Madigan@yahoo.co.jp>
2019-04-22 11:26:25 +0900

    修正参数说明文档错误
    send_text_card函数的title参数和user_ids参数说明错误

--- wechatpy/enterprise/client/api/message.py
+++ wechatpy/enterprise/client/api/message.py
@@ -99,8 +99,8 @@ class WeChatMessage(BaseWeChatAPI):
         具体用法请参考上面的示例。
 
         :param agent_id: 必填，企业应用的id，整型。可在应用的设置页面查看。
-        :param user_ids: 成员ID列表。
-        :param title: 必填，成员ID列表（消息接收者，多个接收者用‘|’分隔，最多支持1000个）。
+        :param user_ids: 成员ID列表（消息接收者，多个接收者用‘|’分隔，最多支持1000个）。
+        :param title: 标题，不超过128个字节，超过会自动截断。
         :param description: 必填，描述，不超过512个字节，超过会自动截断
         :param url: 必填，点击后跳转的链接。
         :param btntxt: 按钮文字。 默认为“详情”， 不超过4个文字，超过自动截断。
