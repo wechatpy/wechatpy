1f2a03410cd4374d0ba7b793301f5dee5f772e0e
Chris Guo <41265033+chrisguox@users.noreply.github.com>
2019-07-20 14:59:18 +0800

    feat: 微信支付退款增加退款说明字段 (#476)

--- wechatpy/pay/api/refund.py
+++ wechatpy/pay/api/refund.py
@@ -9,7 +9,7 @@ class WeChatRefund(BaseWeChatPayAPI):
     def apply(self, total_fee, refund_fee, out_refund_no, transaction_id=None,
               out_trade_no=None, fee_type='CNY', op_user_id=None,
               device_info=None, refund_account='REFUND_SOURCE_UNSETTLED_FUNDS',
-              notify_url=None):
+              refund_desc=None, notify_url=None):
         """
         申请退款
 
@@ -21,6 +21,7 @@ class WeChatRefund(BaseWeChatPayAPI):
         :param fee_type: 可选，货币类型，符合ISO 4217标准的三位字母代码，默认人民币：CNY
         :param op_user_id: 可选，操作员帐号, 默认为商户号
         :param device_info: 可选，终端设备号
+        :param refund_desc: 可选，退款原因
         :param refund_account: 可选，退款资金来源，仅针对老资金流商户使用，默认使用未结算资金退款
         :param notify_url: 可选，异步接收微信支付退款结果通知的回调地址
         :return: 返回的结果数据
@@ -36,6 +37,7 @@ class WeChatRefund(BaseWeChatPayAPI):
             'refund_fee_type': fee_type,
             'op_user_id': op_user_id if op_user_id else self.mch_id,
             'refund_account': refund_account,
+            "refund_desc": refund_desc,
             'notify_url': notify_url,
         }
         return self._post('secapi/pay/refund', data=data)
