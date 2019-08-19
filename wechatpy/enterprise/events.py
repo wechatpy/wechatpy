910b0550c5d26737c80a558f00eda0287cb64c15
renmu <1101022351@qq.com>
2019-08-19 15:20:20 +0800

    更新了企业微信接收事件官方文档链接的注释

--- wechatpy/enterprise/events.py
+++ wechatpy/enterprise/events.py
@@ -20,7 +20,7 @@ class SubscribeEvent(events.SubscribeEvent):
     """
     成员关注事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    https://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E6.88.90.E5.91.98.E5.85.B3.E6.B3.A8.2F.E5.8F.96.E6.B6.88.E5.85.B3.E6.B3.A8.E4.BA.8B.E4.BB.B6
     """
     agent = IntegerField('AgentID', 0)
 
@@ -30,7 +30,7 @@ class UnsubscribeEvent(events.UnsubscribeEvent):
     """
     成员取消关注事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    https://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E6.88.90.E5.91.98.E5.85.B3.E6.B3.A8.2F.E5.8F.96.E6.B6.88.E5.85.B3.E6.B3.A8.E4.BA.8B.E4.BB.B6
     """
     agent = IntegerField('AgentID', 0)
 
@@ -40,7 +40,7 @@ class ClickEvent(events.ClickEvent):
     """
     点击菜单拉取消息事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E7.82.B9.E5.87.BB.E8.8F.9C.E5.8D.95.E6.8B.89.E5.8F.96.E6.B6.88.E6.81.AF.E7.9A.84.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     agent = IntegerField('AgentID', 0)
 
@@ -50,7 +50,7 @@ class ViewEvent(events.ViewEvent):
     """
     点击菜单跳转链接事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E7.82.B9.E5.87.BB.E8.8F.9C.E5.8D.95.E8.B7.B3.E8.BD.AC.E9.93.BE.E6.8E.A5.E7.9A.84.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     agent = IntegerField('AgentID', 0)
 
@@ -60,7 +60,7 @@ class LocationEvent(events.LocationEvent):
     """
     上报地理位置事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E4.B8.8A.E6.8A.A5.E5.9C.B0.E7.90.86.E4.BD.8D.E7.BD.AE.E4.BA.8B.E4.BB.B6
     """
     agent = IntegerField('AgentID', 0)
 
@@ -70,7 +70,7 @@ class ScanCodePushEvent(events.ScanCodePushEvent):
     """
     扫码推事件的事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E6.89.AB.E7.A0.81.E6.8E.A8.E4.BA.8B.E4.BB.B6.E7.9A.84.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     agent = IntegerField('AgentID', 0)
 
@@ -80,7 +80,7 @@ class ScanCodeWaitMsgEvent(events.ScanCodeWaitMsgEvent):
     """
     扫码推事件且弹出“消息接收中”提示框的事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E6.89.AB.E7.A0.81.E6.8E.A8.E4.BA.8B.E4.BB.B6.E4.B8.94.E5.BC.B9.E5.87.BA.E2.80.9C.E6.B6.88.E6.81.AF.E6.8E.A5.E6.94.B6.E4.B8.AD.E2.80.9D.E6.8F.90.E7.A4.BA.E6.A1.86.E7.9A.84.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     agent = IntegerField('AgentID', 0)
 
@@ -90,7 +90,7 @@ class PicSysPhotoEvent(events.PicSysPhotoEvent):
     """
     弹出系统拍照发图事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E5.BC.B9.E5.87.BA.E7.B3.BB.E7.BB.9F.E6.8B.8D.E7.85.A7.E5.8F.91.E5.9B.BE.E7.9A.84.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     agent = IntegerField('AgentID', 0)
 
@@ -100,7 +100,7 @@ class PicPhotoOrAlbumEvent(events.PicPhotoOrAlbumEvent):
     """
     弹出拍照或相册发图事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E5.BC.B9.E5.87.BA.E6.8B.8D.E7.85.A7.E6.88.96.E8.80.85.E7.9B.B8.E5.86.8C.E5.8F.91.E5.9B.BE.E7.9A.84.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     agent = IntegerField('AgentID', 0)
 
@@ -110,7 +110,7 @@ class PicWeChatEvent(events.PicWeChatEvent):
     """
     弹出微信相册发图器事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E5.BC.B9.E5.87.BA.E5.BE.AE.E4.BF.A1.E7.9B.B8.E5.86.8C.E5.8F.91.E5.9B.BE.E5.99.A8.E7.9A.84.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     agent = IntegerField('AgentID', 0)
 
@@ -120,7 +120,7 @@ class LocationSelectEvent(events.LocationSelectEvent):
     """
     弹出地理位置选择器事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E5.BC.B9.E5.87.BA.E5.9C.B0.E7.90.86.E4.BD.8D.E7.BD.AE.E9.80.89.E6.8B.A9.E5.99.A8.E7.9A.84.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     agent = IntegerField('AgentID', 0)
 
@@ -130,7 +130,7 @@ class EnterAgentEvent(events.BaseEvent):
     """
     用户进入应用的事件推送
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E6.88.90.E5.91.98.E8.BF.9B.E5.85.A5.E5.BA.94.E7.94.A8.E7.9A.84.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     agent = IntegerField('AgentID', 0)
     event = 'enter_agent'
@@ -141,7 +141,7 @@ class BatchJobResultEvent(events.BaseEvent):
     """
     异步任务完成事件
     详情请参阅
-    http://qydev.weixin.qq.com/wiki/index.php?title=接受事件
+    http://qydev.weixin.qq.com/wiki/index.php?title=接收事件#.E5.BC.82.E6.AD.A5.E4.BB.BB.E5.8A.A1.E5.AE.8C.E6.88.90.E4.BA.8B.E4.BB.B6.E6.8E.A8.E9.80.81
     """
     event = 'batch_job_result'
     batch_job = BaseField('BatchJob')
