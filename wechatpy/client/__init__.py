5eb3f837e6da92fdbf1a578781345c40f7c49530
machan <macha@minderp.com>
2019-08-29 10:43:47 +0800

    wxopen

--- wechatpy/client/__init__.py
+++ wechatpy/client/__init__.py
@@ -40,6 +40,7 @@ class WeChatClient(BaseWeChatClient):
     wifi = api.WeChatWiFi()
     wxa = api.WeChatWxa()
     marketing = api.WeChatMarketing()
+    wxopen = api.WeChatWeOpen()
 
     def __init__(self, appid, secret, access_token=None,
                  session=None, timeout=None, auto_retry=True):
--- wechatpy/client/api/wxopen.py
+++ wechatpy/client/api/wxopen.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python
 # -*- encoding: UTF-8 -*-
 
+from __future__ import absolute_import, unicode_literals
+
+from optionaldict import optionaldict
+
+from wechatpy.client.api.base import BaseWeChatAPI
+
+
+class WeChatWeOpen(BaseWeChatAPI):
+    API_BASE_URL = 'https://api.weixin.qq.com/'
+
+    def get_wxamp_link(self, path, width=430):
+        """
+        获取公众号关联的小程序
+        详情请参考
+        https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=open1513255108_gFkRI&token=&lang=zh_CN
+        """
+        return self._post(
+            'cgi-bin/wxopen/wxamplinkget'
+        )
