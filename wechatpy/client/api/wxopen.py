41dc92883c0671acf4bc4c575360bb97303f65a6
machan <macha@minderp.com>
2019-08-29 10:53:32 +0800

    wxopen

--- wechatpy/client/api/wxopen.py
+++ wechatpy/client/api/wxopen.py
@@ -11,12 +11,13 @@ from wechatpy.client.api.base import BaseWeChatAPI
 class WeChatWeOpen(BaseWeChatAPI):
     API_BASE_URL = 'https://api.weixin.qq.com/'
 
-    def get_wxamp_link(self, path, width=430):
+    def get_wxamp_link(self):
         """
         获取公众号关联的小程序
         详情请参考
         https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=open1513255108_gFkRI&token=&lang=zh_CN
         """
         return self._post(
-            'cgi-bin/wxopen/wxamplinkget'
+            'cgi-bin/wxopen/wxamplinkget',
+            data={}
         )
