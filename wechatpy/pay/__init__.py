c31536516bdbf042ed295db6ffde679e29afd091
shanguo <shanguoo@gmail.com>
2019-11-14 00:43:31 +0800

    refactor: 优化 WeChatPay 冗余代码 (#488)

--- wechatpy/pay/__init__.py
+++ wechatpy/pay/__init__.py
@@ -106,11 +106,9 @@ class WeChatPay(object):
         else:
             url = url_or_endpoint
 
-        if isinstance(kwargs.get('data', ''), dict):
+        if isinstance(kwargs.get('data'), dict):
             data = kwargs['data']
-            if 'mchid' not in data:
-                # Fuck Tencent
-                data.setdefault('mch_id', self.mch_id)
+            data.setdefault('mch_id', self.mch_id)
             data.setdefault('sub_mch_id', self.sub_mch_id)
             data.setdefault('nonce_str', random_string(32))
             data = optionaldict(data)
