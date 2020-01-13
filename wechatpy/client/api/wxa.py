bfb14fa2982b9309c72e7b7b70b1f5fc215a84fd
ZHOU Cheng <c.zhou@live.com>
2020-01-13 17:27:44 +0800

    增加 check_image_security 和 check_text_security 接口；更新了卡券相关的事件类型和文档。 (#503)
    * merge 474
    
    * merge 455
    
    * remove dup code
    
    * update docs url
    
    * rename function name

--- wechatpy/client/api/wxa.py
+++ wechatpy/client/api/wxa.py
@@ -472,3 +472,35 @@ class WeChatWxa(BaseWeChatAPI):
                 'grant_type': 'authorization_code'
             }
         )
+
+    def check_image_security(self, media):
+        """
+        校验一张图片是否含有违法违规内容。
+        详情请参考
+        https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/sec-check/security.imgSecCheck.html
+
+        :param media: 要检测的图片文件，格式支持PNG、JPEG、JPG、GIF，图片尺寸不超过 750px x 1334px
+        :return:
+        """
+        return self._post(
+            'wxa/img_sec_check',
+            data={
+                'media': open(media),
+            }
+        )
+
+    def check_text_security(self, content):
+        """
+        检查一段文本是否含有违法违规内容。
+        详情请参考
+        https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/sec-check/security.msgSecCheck.html
+
+        :param content: 要检测的文本内容，长度不超过 500KB
+        :return:
+        """
+        return self._post(
+            'wxa/msg_sec_check',
+            data={
+                'content': content,
+            }
+        )
