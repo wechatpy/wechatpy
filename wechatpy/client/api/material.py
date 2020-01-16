e30ff59c47e6b2f65c33ed4923393db9de291325
Howie Zhao <howiezhaohr@hotmail.com>
2020-01-16 11:08:55 +0800

    fix: update WeChat document link (#505)

--- wechatpy/client/api/material.py
+++ wechatpy/client/api/material.py
@@ -10,7 +10,7 @@ class WeChatMaterial(BaseWeChatAPI):
         """
         新增永久图文素材
         详情请参考
-        https://mp.weixin.qq.com/wiki?id=mp1494572718_WzHIY
+        https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Adding_Permanent_Assets.html
 
         :param articles: 图文素材数组
         :type articles: list[dict]
@@ -40,7 +40,7 @@ class WeChatMaterial(BaseWeChatAPI):
         """
         新增其它类型永久素材
         详情请参考
-        http://mp.weixin.qq.com/wiki/14/7e6c03263063f4813141c3e17dd4350a.html
+        https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Adding_Permanent_Assets.html
 
         :param media_type: 媒体文件类型，分别有图片（image）、语音（voice）、视频（video）和缩略图（thumb）
         :param media_file: 要上传的文件，一个 File-object
@@ -72,7 +72,7 @@ class WeChatMaterial(BaseWeChatAPI):
         """
         获取永久素材
         详情请参考
-        http://mp.weixin.qq.com/wiki/4/b3546879f07623cb30df9ca0e420a5d0.html
+        https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Getting_Permanent_Assets.html
 
         :param media_id: 素材的 media_id
         :return: 图文素材返回图文列表，其它类型为素材的内容
@@ -97,7 +97,7 @@ class WeChatMaterial(BaseWeChatAPI):
         """
         删除永久素材
         详情请参考
-        http://mp.weixin.qq.com/wiki/5/e66f61c303db51a6c0f90f46b15af5f5.html
+        https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Deleting_Permanent_Assets.html
 
         :param media_id: 素材的 media_id
         :return: 返回的 JSON 数据包
@@ -113,7 +113,7 @@ class WeChatMaterial(BaseWeChatAPI):
         """
         修改永久图文素材
         详情请参考
-        https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1444738732
+        https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Editing_Permanent_Rich_Media_Assets.html
 
         :param media_id: 要修改的图文消息的 id
         :param index: 要更新的文章在图文消息中的位置（多图文消息时，此字段才有意义），第一篇为 0
@@ -142,7 +142,7 @@ class WeChatMaterial(BaseWeChatAPI):
         """
         修改永久图文素材
         详情请参考
-        http://mp.weixin.qq.com/wiki/4/19a59cba020d506e767360ca1be29450.html
+        https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Editing_Permanent_Rich_Media_Assets.html
 
         :param media_id: 要修改的图文消息的 id
         :param index: 要更新的文章在图文消息中的位置（多图文消息时，此字段才有意义），第一篇为 0
@@ -155,7 +155,7 @@ class WeChatMaterial(BaseWeChatAPI):
         """
         批量获取永久素材列表
         详情请参考
-        http://mp.weixin.qq.com/wiki/12/2108cd7aafff7f388f41f37efa710204.html
+        https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Get_materials_list.html
 
         :param media_type: 媒体文件类型，分别有图片（image）、语音（voice）、视频（video）和缩略图（news）
         :param offset: 从全部素材的该偏移位置开始返回，0 表示从第一个素材返回
@@ -175,7 +175,7 @@ class WeChatMaterial(BaseWeChatAPI):
         """
         获取素材总数
         详情请参考
-        http://mp.weixin.qq.com/wiki/16/8cc64f8c189674b421bee3ed403993b8.html
+        https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Get_the_total_of_all_materials.html
 
         :return: 返回的 JSON 数据包
         """
