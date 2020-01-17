59c34e07aad6d7d95e3e8c65b0318b1ee2bd5c7d
nanshan03 <734463238@qq.com>
2020-01-17 11:38:31 +0800

    fix:调整测试用例符合标准

--- tests/test_external_contact.py
+++ tests/test_external_contact.py
@@ -3,13 +3,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import os
 import unittest
-from datetime import datetime
-
-import six
 from httmock import HTTMock, response, urlmatch
-
 from wechatpy.enterprise import WeChatClient
-from wechatpy.exceptions import WeChatClientException
 from wechatpy.utils import json
 
 _TESTS_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -47,14 +42,13 @@ class WeChatClientTestCase(unittest.TestCase):
     def setUp(self):
         self.client = WeChatClient(self.app_id, self.secret)
 
-
     def test_ec_addcorptag(self):
         tags = [{
             "name": "大鸟"
         },
-        {
-            "name": "小菜"
-        }]
+            {
+                "name": "小菜"
+            }]
         with HTTMock(wechat_api_mock):
             res = self.client.external_contact.add_corp_tag(None, "开发3组", 1, tags=tags)
         self.assertEqual(0, res['errcode'])
@@ -63,9 +57,9 @@ class WeChatClientTestCase(unittest.TestCase):
         tags = [{
             "name": "大鸟"
         },
-        {
-            "name": "小菜"
-        }]
+            {
+                "name": "小菜"
+            }]
         with HTTMock(wechat_api_mock):
             res = self.client.external_contact.add_corp_tag(None, "开发3组", 1, tags=tags)
         self.assertEqual(0, res['errcode'])
@@ -81,8 +75,7 @@ class WeChatClientTestCase(unittest.TestCase):
         self.assertEqual(0, res['errcode'])
 
     def test_ec_mark_tag(self):
-
         with HTTMock(wechat_api_mock):
             res = self.client.external_contact.mark_tag('zm', 'wmm7wjCgAAkLAv_eiVt53eBokOC3_Tww',
-                                                         add_tag=['etm7wjCgAA40ptIZTBWOO0C_RXoY_q3g'])
+                                                        add_tag=['etm7wjCgAA40ptIZTBWOO0C_RXoY_q3g'])
         self.assertEqual(0, res['errcode'])
