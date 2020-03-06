1a3cac09ca8e469578502ce32133ee4900b98295
messense <messense@icloud.com>
2020-03-06 21:44:00 +0800

    Bump version to 1.18.11.alpha1

--- wechatpy/__init__.py
+++ wechatpy/__init__.py
@@ -10,7 +10,7 @@ from wechatpy.parser import parse_message  # NOQA
 from wechatpy.pay import WeChatPay  # NOQA
 from wechatpy.replies import create_reply  # NOQA
 
-__version__ = '1.8.10'
+__version__ = '1.8.11.alpha1'
 __author__ = 'messense'
 
 # Set default logging handler to avoid "No handler found" warnings.
