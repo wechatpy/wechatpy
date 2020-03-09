846739aac09db272047e104972771593bc716c85
messense <messense@icloud.com>
2020-03-09 16:02:59 +0800

    Bump version: 1.8.11 → 1.8.12

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='wechatpy',
-    version='1.8.11',
+    version='1.8.12',
     author='messense',
     author_email='messense@icloud.com',
     url='https://github.com/jxtech/wechatpy',
