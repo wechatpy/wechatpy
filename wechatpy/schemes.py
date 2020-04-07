f7d44b6dc910a4b67abfe4257ffba8b8b39bf7a3
provefar <provefars@gmail.com>
2020-04-07 17:40:56 +0800

    添加数据类基类及其测试

--- wechatpy/schemes.py
+++ wechatpy/schemes.py
@@ -1,10 +1,34 @@
-from pydantic import BaseModel
 from time import time
 from wechatpy.utils import random_string
 from json import dumps
+import warnings
 
 
-class JsapiCardExt(BaseModel):
+class DataclassesBase:
+    def __init__(self, **data):
+        annotations = self.__annotations__.copy() if hasattr(self, "__annotations__") else {}
+        for key, value in data.items():
+            if key in annotations:
+                if isinstance(value, annotations[key]):
+                    self.__dict__[key] = value
+                    annotations.pop(key)
+                else:
+                    raise TypeError(
+                        "The type of value of %s should be %s, but got %s" % (key, annotations[key], type(value))
+                    )
+            else:
+                warnings.warn("Got an unexpected key %s" % key)
+
+        for annotation in annotations:
+            error_msg = []
+            if not hasattr(self, annotation):
+                error_msg.append("\tname: %s, type: %s" % (annotation, annotations[annotation]))
+
+            if error_msg:
+                raise NameError("There are some missing values\n" + "\n\b".join(error_msg))
+
+
+class JsapiCardExt(DataclassesBase):
     """本类用于指示 jsapi 中批量添加卡券接口的某个参数格式.
 
     文档中要求本结构应为 JSON 字符串, 因此使用时注意转换.
