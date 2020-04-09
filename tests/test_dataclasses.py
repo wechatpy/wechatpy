44c359f44496db106ae0ee2f229c971b9c2f2188
provefar <provefars@gmail.com>
2020-04-09 10:06:47 +0800

    新增批量添加卡券接口 (#556)
    * 新增"添加卡券接口"一参数格式
    * 新增"添加卡券接口"参数生成
    * 整理 card_ext 生成细节
    * 使用 black 代码格式化, 添加新的依赖包
    * 添加数据类基类及其测试
    * 为数据类添加setter检测/todict及其测试
    * 覆写CardExt dict*()方法并测试
    * 修改`get_jsapi_add_card_params`拼写并添加其测试
    * 删除不必要的依赖
    * 优化 schemas 文件内导入顺序

---
+++ tests/test_dataclasses.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import unittest
+from wechatpy.schemes import DataclassesBase
+import warnings
+from wechatpy.utils import random_string
+
+
+class DataclassesBaseTestCase(unittest.TestCase):
+    class A(DataclassesBase):
+        a: int
+        b: str
+        c: dict
+        d: str = "2"
+
+    def test_dataclasses_init(self):
+        a = 1
+        b = random_string()
+        c = {random_string(): random_string()}
+
+        a1 = self.A(a=a, b=b, c=c)
+        assert a1.a == a
+        assert a1.b == b
+        assert a1.c == c
+        assert a1.d == self.A.d
+
+    def test_dataclasses_todict(self):
+        a = 1
+        b = random_string()
+        c = {random_string(): random_string()}
+
+        a1 = self.A(a=a, b=b, c=c)
+        assert a1.dict() == {"a": a, "b": b, "c": c, "d": self.A.d}
+
+    def test_unexpected_key(self):
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            random_key = random_string()
+
+            self.A(a=2, b="asd", c={}, **{random_key: random_string()})
+            assert "Got an unexpected key %s" % random_key == str(w[-1].message)
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            random_key = random_string()
+
+            a1 = self.A(a=2, b="asd", c={})
+            setattr(a1, random_key, random_string())
+            assert "Got an unexpected key %s" % random_key == str(w[-1].message)
+
+    def test_type_error(self):
+        try:
+            self.A(a=2, b="asd", c={}, d=2)
+        except Exception as e:
+            assert "The type of value of d should be <class 'str'>, but got <class 'int'>" in str(e)
+        else:
+            assert "Here shoule be an TypeError, but failed to trigger it" and False
+
+    def test_set_type_error(self):
+        a1 = self.A(a=2, b="asd", c={})
+        try:
+            a1.d = 2
+        except Exception as e:
+            assert "The type of value of d should be <class 'str'>, but got <class 'int'>" in str(e)
+        else:
+            assert "Here shoule be an TypeError, but failed to trigger it" and False
+
+    def test_missing_values(self):
+        try:
+            self.A(a=2, c={})
+        except Exception as e:
+            assert "There are some missing values" in str(e)
+        else:
+            assert "Here shoule be an NameError, but failed to trigger it" and False
