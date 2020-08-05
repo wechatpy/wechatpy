08552d4f974c1d1968441488a4fc73b202ff9608
ZHOU Cheng <c.zhou@live.com>
2020-08-05 19:33:10 +0800

    update poi doc

--- wechatpy/client/api/poi.py
+++ wechatpy/client/api/poi.py
@@ -4,12 +4,17 @@ from wechatpy.client.api.base import BaseWeChatAPI
 
 
 class WeChatPoi(BaseWeChatAPI):
+    """ 微信门店
+
+    https://developers.weixin.qq.com/doc/offiaccount/WeChat_Stores/WeChat_Store_Interface.html
+    """
+
     def add(self, poi_data):
         """
         创建门店
 
         详情请参考
-        http://mp.weixin.qq.com/wiki/16/8f182af4d8dcea02c56506306bdb2f4c.html
+        https://developers.weixin.qq.com/doc/offiaccount/WeChat_Stores/WeChat_Store_Interface.html#7
 
         :param poi_data: 门店信息字典
         :return: 返回的 JSON 数据包
@@ -21,7 +26,7 @@ class WeChatPoi(BaseWeChatAPI):
         查询门店信息
 
         详情请参考
-        http://mp.weixin.qq.com/wiki/16/8f182af4d8dcea02c56506306bdb2f4c.html
+        https://developers.weixin.qq.com/doc/offiaccount/WeChat_Stores/WeChat_Store_Interface.html#9
 
         :param poi_id: 门店 ID
         :return: 返回的 JSON 数据包
@@ -33,20 +38,20 @@ class WeChatPoi(BaseWeChatAPI):
         查询门店列表
 
         详情请参考
-        http://mp.weixin.qq.com/wiki/16/8f182af4d8dcea02c56506306bdb2f4c.html
+        https://developers.weixin.qq.com/doc/offiaccount/WeChat_Stores/WeChat_Store_Interface.html#10
 
         :param begin: 开始位置，0 即为从第一条开始查询
         :param limit: 返回数据条数，最大允许50，默认为20
         :return: 返回的 JSON 数据包
         """
-        return self._post("poi/getpoilist", data={"begin": begin, "limit": limit,})
+        return self._post("poi/getpoilist", data={"begin": begin, "limit": limit})
 
     def update(self, poi_data):
         """
-        修改门店
+        修改门店服务信息
 
         详情请参考
-        http://mp.weixin.qq.com/wiki/16/8f182af4d8dcea02c56506306bdb2f4c.html
+        https://developers.weixin.qq.com/doc/offiaccount/WeChat_Stores/WeChat_Store_Interface.html#11
 
         :param poi_data: 门店信息字典
         :return: 返回的 JSON 数据包
@@ -58,7 +63,7 @@ class WeChatPoi(BaseWeChatAPI):
         删除门店
 
         详情请参考
-        http://mp.weixin.qq.com/wiki/16/8f182af4d8dcea02c56506306bdb2f4c.html
+        https://developers.weixin.qq.com/doc/offiaccount/WeChat_Stores/WeChat_Store_Interface.html#12
 
         :param poi_id: 门店 ID
         :return: 返回的 JSON 数据包
@@ -70,7 +75,7 @@ class WeChatPoi(BaseWeChatAPI):
         获取微信门店类目表
 
         详情请参考
-        http://mp.weixin.qq.com/wiki/16/8f182af4d8dcea02c56506306bdb2f4c.html
+        https://developers.weixin.qq.com/doc/offiaccount/WeChat_Stores/WeChat_Store_Interface.html#13
 
         :return: 门店类目表
         """
