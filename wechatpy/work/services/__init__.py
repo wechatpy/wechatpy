c38a6051b2f70e5c42e1f8b9b1554c585790ffcc
WeijiangChen <ourweijiang@gmail.com>
2021-11-04 16:57:34 +0800

    修正suite_access_token

--- wechatpy/work/services/__init__.py
+++ wechatpy/work/services/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import json
 import time
 import logging
 import requests
@@ -11,6 +11,9 @@ logger = logging.getLogger(__name__)
 
 
 class WeChatServiceClient(BaseWeChatClient):
+    """
+    注意：access_token在第三方应用变更为suite_access_token参数
+    """
     API_BASE_URL = "https://qyapi.weixin.qq.com/cgi-bin/"
 
     auth = api.WeChatAuth()
@@ -63,9 +66,41 @@ class WeChatServiceClient(BaseWeChatClient):
         expires_in = 7200
         if "expires_in" in result:
             expires_in = result["expires_in"]
-        self.session.set(self.access_token_key, result["access_token"], expires_in)
+        self.session.set(self.access_token_key, result["suite_access_token"], expires_in)
         self.expires_at = int(time.time()) + expires_in
         return result
+
+    def _request(self, method, url_or_endpoint, **kwargs):
+        if not url_or_endpoint.startswith(("http://", "https://")):
+            api_base_url = kwargs.pop("api_base_url", self.API_BASE_URL)
+            url = f"{api_base_url}{url_or_endpoint}"
+        else:
+            url = url_or_endpoint
+
+        if "params" not in kwargs:
+            kwargs["params"] = {}
+        if isinstance(kwargs["params"], dict) and "suite_access_token" not in kwargs["params"]:
+            kwargs["params"]["suite_access_token"] = self.access_token
+        if isinstance(kwargs.get("data", ""), dict):
+            body = json.dumps(kwargs["data"], ensure_ascii=False)
+            body = body.encode("utf-8")
+            kwargs["data"] = body
+
+        kwargs["timeout"] = kwargs.get("timeout", self.timeout)
+        result_processor = kwargs.pop("result_processor", None)
+        res = self._http.request(method=method, url=url, **kwargs)
+        try:
+            res.raise_for_status()
+        except requests.RequestException as reqe:
+            raise WeChatClientException(
+                errcode=None,
+                errmsg=None,
+                client=self,
+                request=reqe.request,
+                response=reqe.response,
+            )
+
+        return self._handle_result(res, method, url, result_processor, **kwargs)
 
     def fetch_access_token(self):
         """Fetch access token"""
