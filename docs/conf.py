caaba227dc5ab08a3d75ef4cebec5ab81890a742
messense <messense@icloud.com>
2021-11-09 10:32:02 +0800

    Fix documentation generation on RtD (#674)

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,6 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.append(os.path.abspath("_themes"))
-sys.path.insert(0, os.path.abspath(".."))
 
 import wechatpy
 
