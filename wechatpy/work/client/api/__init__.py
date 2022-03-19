2f6e65dc095465549d240c2c9edeb8d5dbde877c
yaoqiankun <yaoqiankun@datarc.cn>
2022-03-19 19:27:14 +0800

    重命名变量

--- wechatpy/work/client/api/__init__.py
+++ wechatpy/work/client/api/__init__.py
@@ -6,11 +6,11 @@ from wechatpy.work.client.api.appchat import WeChatAppChat  # NOQA
 from wechatpy.work.client.api.batch import WeChatBatch  # NOQA
 from wechatpy.work.client.api.calendar import WeChatCalendar  # NOQA
 from wechatpy.work.client.api.department import WeChatDepartment  # NOQA
+from wechatpy.work.client.api.email import WeChatEMail  # NOQA
 from wechatpy.work.client.api.external_contact import WeChatExternalContact  # NOQA
 from wechatpy.work.client.api.external_contact_group_chat import WeChatExternalContactGroupChat
 from wechatpy.work.client.api.invoice import WeChatInvoice  # NOQA
 from wechatpy.work.client.api.jsapi import WeChatJSAPI  # NOQA
-from wechatpy.work.client.api.mail import WeChatEMail  # NOQA
 from wechatpy.work.client.api.media import WeChatMedia  # NOQA
 from wechatpy.work.client.api.menu import WeChatMenu  # NOQA
 from wechatpy.work.client.api.message import WeChatMessage  # NOQA
