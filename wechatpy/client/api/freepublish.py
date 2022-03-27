eb9e1f217310506f4afbbf6e00d0eb9eda27d5df
ZHOU Cheng <c.zhou@live.com>
2022-03-27 15:18:54 +0800

    add delete

--- wechatpy/client/api/freepublish.py
+++ wechatpy/client/api/freepublish.py
@@ -26,3 +26,17 @@ class WeChatFreePublish(BaseWeChatAPI):
         :return: 返回的 JSON 数据包
         """
         return self._post("freepublish/get", data={"publish_id": publish_id})
+
+    def delete(self, article_id: str, index: int = 0) -> dict:
+        """
+        删除发布
+        发布成功之后，随时可以通过该接口删除。此操作不可逆，请谨慎操作。
+
+        详情请参考：
+        https://developers.weixin.qq.com/doc/offiaccount/Publish/Delete_posts.html
+
+        :param article_id: 成功发布时返回的 article_id
+        :param index: 要删除的文章在图文消息中的位置，第一篇编号为1，该字段不填或填 0 会删除全部文章
+        :return: 返回的 JSON 数据包
+        """
+        return self._post("freepublish/delete", data={"article_id": article_id, "index": index})
