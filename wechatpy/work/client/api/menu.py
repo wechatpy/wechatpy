6fbcb0b90a886723e7b42b325c207faffb80d7b8
yaoqiankun <yaoqiankun@datarc.cn>
2022-04-06 23:19:16 +0800

    更新 WeChatMenu

--- wechatpy/work/client/api/menu.py
+++ wechatpy/work/client/api/menu.py
@@ -7,40 +7,41 @@ from wechatpy.client.api.base import BaseWeChatAPI
 class WeChatMenu(BaseWeChatAPI):
     """
     自定义菜单
-
-    https://work.weixin.qq.com/api/doc#90000/90135/90230
     """
 
-    def create(self, agent_id, menu_data):
+    def create(self, agent_id: int, menu_data: dict) -> dict:
         """
         创建菜单
 
-        https://work.weixin.qq.com/api/doc#90000/90135/90231
+        详情请参考：
+        https://developer.work.weixin.qq.com/document/path/90231
 
         :param agent_id: 应用id
         """
         return self._post("menu/create", params={"agentid": agent_id}, data=menu_data)
 
-    def get(self, agent_id):
+    def get(self, agent_id: int) -> dict:
         """
         获取菜单
 
-        https://work.weixin.qq.com/api/doc#90000/90135/90232
+        详情请参考：
+        https://developer.work.weixin.qq.com/document/path/90232
 
         :param agent_id: 应用id
         """
         return self._get("menu/get", params={"agentid": agent_id})
 
-    def delete(self, agent_id):
+    def delete(self, agent_id: int) -> dict:
         """
         删除菜单
 
-        https://work.weixin.qq.com/api/doc#90000/90135/90233
+        详情请参考：
+        https://developer.work.weixin.qq.com/document/path/90233
 
         :param agent_id: 应用id
         """
         return self._get("menu/delete", params={"agentid": agent_id})
 
-    def update(self, agent_id, menu_data):
+    def update(self, agent_id: int, menu_data: dict) -> dict:
         self.delete(agent_id)
         return self.create(agent_id, menu_data)
