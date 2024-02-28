c4e0232c4755ce40550417adb6497891956756b9
Vainl <vainl@qq.com>
2024-02-28 17:09:00 +0800

    添加小程序获取加密URLLink/查询加密URLLink接口

--- wechatpy/client/api/wxa.py
+++ wechatpy/client/api/wxa.py
@@ -491,6 +491,62 @@ class WeChatWxa(BaseWeChatAPI):
             },
         )
 
+    def generate_url_link(
+        self,
+        path,
+        query="",
+        expire_type=0,
+        expire_time=None,
+        expire_interval=0,
+        cloud_base=None,
+        env_version="release",
+    ):
+        """
+        获取加密URLLink，获取小程序 URL Link，适用于短信、邮件、网页、微信内等拉起小程序的业务场景。
+        详情请参考
+        https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/qrcode-link/url-link/generateUrlLink.html
+
+        :param path: 通过 URL Link 进入的小程序页面路径，必须是已经发布的小程序存在的页面，不可携带 query 。path 为空时会跳转小程序主页
+        :param query: 通过 URL Link 进入小程序时的query，最大1024个字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~%
+        :param expire_type: 默认值0.小程序 URL Link 失效类型，失效时间：0，失效间隔天数：1
+        :param expire_time: 到期失效的 URL Link 的失效时间，为 Unix 时间戳。生成的到期失效 URL Link 在该时间前有效。最长有效期为30天。expire_type 为 0 必填
+        :param expire_interval: 到期失效的URL Link的失效间隔天数。生成的到期失效URL Link在该间隔时间到达前有效。最长间隔天数为30天。expire_type 为 1 必填
+        :param cloud_base: 云开发静态网站自定义 H5 配置参数，可配置中转的云开发 H5 页面。不填默认用官方 H5 页面
+        :param env_version: 默认值"release"。要打开的小程序版本。正式版为 "release"，体验版为"trial"，开发版为"develop"，仅在微信外打开时生效。
+        :return: 返回的 JSON 数据包
+        """
+        return self._post(
+            "wxa/generate_urllink",
+            data={
+                "path": path,
+                "query": query,
+                "is_expire": True,
+                "expire_type": expire_type,
+                "expire_time": expire_time,
+                "expire_interval": expire_interval,
+                "cloud_base": cloud_base,
+                "env_version": env_version,
+            },
+        )
+
+    def query_url_link(self, url_link, query_type=0):
+        """
+        查询加密URLLink，该接口用于查询小程序加密 url_link 配置
+        详情请参考
+        https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/qrcode-link/url-link/queryUrlLink.html
+
+        :param url_link: 小程序 url_link
+        :param query_type: 查询类型。默认值0
+        :return:
+        """
+        return self._post(
+            "wxa/query_urllink",
+            data={
+                "url_link": url_link,
+                "query_type": query_type,
+            },
+        )
+
     def check_image_security(self, media):
         """
         校验一张图片是否含有违法违规内容。
